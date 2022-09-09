9f36cbb764888aa1cbb753bdd58d1506612219c6
Liz Huancapaza H <39383110+hhliz@users.noreply.github.com>
2022-09-09 00:44:05 -0300

    Delete upgit.sh

--- upgit.sh
+++
@@ -1,9 +0,0 @@
-# uploading
-rm -rf .git
-git init
-git config --local user.email hhliz@github.com
-git config --global http.postBuffer 200000000
-git add .
-git commit -m "Initial commit"
-git remote add origin https://github.com/hhliz/pestdata
-git push -u --force origin master
